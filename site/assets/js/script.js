61259e9a98e39577be9e99bdc205b017df9a8aa6
IsmaelJSantos <ismael.js1996@gmail.com>
2025-07-18 14:31:51 -0300

    feat: layout desktop concluido

--- site/assets/js/script.js
+++ site/assets/js/script.js
@@ -1,2 +1,12 @@
 $(document).ready(function () {
+  $(".box-depoimentos").slick({
+    dots: true,
+    arrows: false,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+  });
 });
